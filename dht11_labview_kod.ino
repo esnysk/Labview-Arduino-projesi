0c38d7d5979cef2557328ebc8173a168c8e08bae
esnysk <43849512+esnysk@users.noreply.github.com>
2019-05-13 11:05:12 +0300

    Add files via upload

---
+++ dht11_labview_kod.ino
@@ -0,0 +1,38 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <dht11.h>
+#define DHT11PIN 2
+#define dhttype DHT11 //sensör tipi
+
+dht11 DHT11;
+
+char in;
+int temp ;
+int hum;
+
+void setup() 
+{
+ Serial.begin(9600);
+
+
+}
+
+void loop() 
+{ 
+  if(Serial.available()>0)
+  {
+   in = (byte)Serial.read() & ~(0x20);
+
+   if(in == 'T') // temp döndürmek
+   {
+     temp = dht11.readTemperature();  
+    Serial.println("temp"); 
+   }else if (in == 'H')
+   {
+    hum = DHT11.readHumidity();
+    Seial.println(hum);
+   }  
+  }
+}
+
